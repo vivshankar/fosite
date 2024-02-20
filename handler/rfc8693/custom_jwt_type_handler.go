b3ecf398fe75124caf0fce06acd0bdc4bf567b71
Vivek Shankar <shankarv@sg.ibm.com>
2024-02-20 11:08:42 +0800

    fix: missing 'issued_token_type' and use of the original session to build the normalized subject token object

--- handler/rfc8693/custom_jwt_type_handler.go
+++ handler/rfc8693/custom_jwt_type_handler.go
@@ -91,6 +91,8 @@ func (c *CustomJWTTypeHandler) PopulateTokenEndpointResponse(ctx context.Context
 		return err
 	}
 
+	responder.SetExtra("issued_token_type", requestedTokenType)
+
 	return nil
 }
 
